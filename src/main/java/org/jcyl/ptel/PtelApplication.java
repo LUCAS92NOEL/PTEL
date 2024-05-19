48c3093ddb732691702619a00e3a10fb28ef1764
Noel <NoelTraperoPastor@hotmail.com.>
2024-05-19 18:27:16 +0200

    Primer commit - subiendo proyecto ptel

---
+++ src/main/java/org/jcyl/ptel/PtelApplication.java
@@ -0,0 +1,13 @@
+package org.jcyl.ptel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jcyl/ptel/PtelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jcyl.ptel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
